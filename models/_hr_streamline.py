02d5d871577b526eab205dd56b9e3893b8ce02c1
Florent Pigout <florent@toopy.org>
2013-07-01 19:52:29 +0200

    Computes and stores signature file format on fly when creates when update employee signature.

--- models/_hr_streamline.py
+++ models/_hr_streamline.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from base64 import b64decode
+from cStringIO import StringIO
+from PIL import Image
+
 import openerp
 
 from osv import fields
@@ -12,5 +16,22 @@ class hr_employee_streamline(osv.osv):
     _inherit = 'hr.employee'
 
     _columns ={
-        'signature' : fields.binary(_('Signature'))
+        'signature' : fields.binary(_('Signature')),
+        'signature_type' : fields.char(_('Signature Type (.png, jpg, ..)')),
     }
+
+    def _update_values(self, values):
+        if not 'signature' in values:
+            return
+        file_like = StringIO(b64decode(values['signature']))
+        values['signature_type'] = Image.open(file_like).format.lower()
+
+    def create(self, cr, uid, values, context=None):
+        self._update_values(values)
+        return super(hr_employee_streamline, self).create(cr, uid, values,
+                                                          context)
+
+    def write(self, cr, uid, ids, values, context=None):
+        self._update_values(values)
+        return super(hr_employee_streamline, self).write(cr, uid, ids, values,
+                                                         context=context)
