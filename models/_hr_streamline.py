f195e515c2f62c3f5565e073fb98157f3c18c69f
Florent Pigout <florent@toopy.org>
2013-07-02 12:16:06 +0200

    Adds user image when image format is invalid and invalids create or write action.

--- models/_hr_streamline.py
+++ models/_hr_streamline.py
@@ -23,8 +23,18 @@ class hr_employee_streamline(osv.osv):
     def _update_values(self, values):
         if not 'signature' in values:
             return
+        # fake file obj
         file_like = StringIO(b64decode(values['signature']))
-        values['signature_type'] = Image.open(file_like).format.lower()
+        # load image and get its format
+        try:
+            format_ = Image.open(file_like).format.lower()
+        # oops
+        except Exception, e:
+            raise osv.except_osv(
+                _('Invalid Image'),
+                _('Image can not be loaded!'))
+        # keep the signature image format
+        values['signature_type'] = format_
 
     def create(self, cr, uid, values, context=None):
         self._update_values(values)
