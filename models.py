6efd0829024050422bab78639cf6b642fee39df8
Emmanuel Cazenave <emmanuel.cazenave@gmail.com>
2013-08-22 18:45:37 +0200

    Add a convenient method to directly get the manager of an employee.

--- models.py
+++ models.py
@@ -14,9 +14,30 @@ class hr_employee_streamline(osv.osv):
 
     _inherit = 'hr.employee'
 
+    def _get_managers(self, cr, uid, ids, field, arg, context=None):
+        """Returns the manager of the user
+        or openerp.osv.orm.browse_null instance
+        if no manager is found
+        """
+        res = dict()
+        for employee in self.browse(cr, uid, ids,context=context):
+            if employee.department_id:
+                manager = employee.department_id.manager_id
+            else:
+                manager = openerp.osv.orm.browse_null()
+            res[employee.id] = manager
+        return res
+
     _columns ={
         'signature' : fields.binary(_('Signature')),
         'signature_type' : fields.char(_('Signature Type (.png, jpg, ..)')),
+        'manager_id': fields.function(
+            _get_managers,
+            method=True,
+            string="Manager",
+            type='many2one',
+            obj='hr.employee',
+            store=False),
     }
 
     def _update_values(self, values):
@@ -38,6 +59,8 @@ class hr_employee_streamline(osv.osv):
                 _('Image can not be loaded!'))
         # keep the signature image format
         values['signature_type'] = format_
+
+
 
     def create(self, cr, uid, values, context=None):
         self._update_values(values)
