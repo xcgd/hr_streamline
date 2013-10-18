47016a47c379ae2dc73d01b8be99a7008db20331
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2013-10-18 18:01:36 +0200

    Bump the version to 1.1

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Human Employee Streamline',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'XCG Consulting',
     'category': 'Human Resources',
     'description': """ enchancements to the hr module to streamline its usage""",
