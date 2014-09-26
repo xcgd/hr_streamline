2e7f4daa914cccfa21b2cdd1ab536c0caea8cdb5
Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
2014-09-26 18:32:10 +0200

    Production release

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Human Employee Streamline',
-    'version': '1.4.1',
+    'version': '1.5',
     'author': 'XCG Consulting',
     'category': 'Human Resources',
     'description': """ enchancements to the hr module to
