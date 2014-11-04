7da32ee406fc6d6cc3e84f4e2bd10f3a4b16977b
Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
2014-11-04 17:28:12 +0100

    Fixed record rules (allow all company linked to user) Allow everyone to read operational department Changed priority to a handle widget in lists.

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Human Employee Streamline',
-    'version': '1.5.1',
+    'version': '1.5.2',
     'author': 'XCG Consulting',
     'category': 'Human Resources',
     'description': """ enchancements to the hr module to
